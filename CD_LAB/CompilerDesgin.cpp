84c617a7b2665328deb8350213f82f0fc755f7e6
Hritik Raj <hritikraj8804@gmail.com>
2025-03-04 11:23:21 +0530

    Add files via upload

---
+++ CD_LAB/CompilerDesgin.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	  int i=23;
+      cout<<"The is my first program";
+      cout<<"\n";
+      for(int i=0;i<5;i++)
+      {
+      	  cout<<i++;
+	  }
+	  
+	  }
