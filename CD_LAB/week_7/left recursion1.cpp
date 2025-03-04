25a516ffc994c022785a088ff1c0e48cb72a97dc
Hritik Raj <hritikraj8804@gmail.com>
2025-03-04 11:29:12 +0530

    Add files via upload

---
+++ CD_LAB/week_7/left recursion1.cpp
@@ -0,0 +1,46 @@
+/*
+E->E+T/T
+T->T*F/F
+F->(E)/id
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+ void removeLeftRecursion(string str){
+ 	int ind = 0;
+ 	string s1="",s2="";
+ 	for(int i =0;i<str.size();i++){
+ 		if(str[i] =='/'){
+ 			ind = i;
+ 			break;
+		 }
+	 }
+	 if(!isupper(str[0]))
+	 {
+	 	cout<<"Invalid Production";
+	 }
+	 else if(str[0] == str[3] && ind !=0){
+	 	s1 = string(1,str[0])+"->"+string(1,str[ind+1])+string(1,str[0])+"'";
+	 	s2 = string(1,str[0])+"'"+"->"+str.substr(4,str.size()-ind)+string(1,str[0])+"'"+string(1,str[ind])+"E";
+	 	cout<<s1<<"\n";
+	 	cout<<s2<<"\n";
+	 }
+	 else
+	 {
+	 	cout<<str<<"\n";
+	 }
+ }
+ int main(){
+ 	int n;
+ 	cout<<"Enter your number of production:";
+ 	cin>>n;
+ 	string str[n];
+ 	cout<<"Enter a grammar:\n";
+ 	for(int i=0;i<n;i++)
+ 	{
+ 		cin>>str[i];
+	 }
+	 for(int i=0;i<n;i++){
+	 	removeLeftRecursion(str[i]);
+	 }
+ }
